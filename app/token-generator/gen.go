f9371eae39af9fdb66f945ad3d802105b157ada7
lejeunel <me@lejeunel.org>
2026-06-07 22:30:01 +0200

    personal access token generator and verifier

---
+++ app/token-generator/gen.go
@@ -0,0 +1,51 @@
+package token_generator
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/base64"
+)
+
+type TokenPair struct {
+	Token string
+	Hash  []byte
+}
+
+type TokenGenerator interface {
+	Generate() (*TokenPair, error)
+	Hash(token string) []byte
+	Verify(string, []byte) bool
+}
+
+type MyTokenGenerator struct {
+	Length int
+}
+
+func NewTokenGenerator(length int) MyTokenGenerator {
+	return MyTokenGenerator{Length: length}
+}
+
+func (g MyTokenGenerator) Generate() (*TokenPair, error) {
+	buf := make([]byte, g.Length)
+	if _, err := rand.Read(buf); err != nil {
+		return nil, err
+	}
+
+	token := base64.RawURLEncoding.EncodeToString(buf)
+	sum := g.Hash(token)
+
+	return &TokenPair{
+		Token: token,
+		Hash:  sum,
+	}, nil
+}
+func (g MyTokenGenerator) Verify(token string, storedHash []byte) bool {
+	computed := sha256.Sum256([]byte(token))
+	return subtle.ConstantTimeCompare(computed[:], storedHash) == 1
+}
+
+func (g MyTokenGenerator) Hash(token string) []byte {
+	sum := sha256.Sum256([]byte(token))
+	return sum[:]
+}
---
+++ app/token-generator/token_generator_test.go
@@ -0,0 +1,20 @@
+package token_generator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// A Base64 encoding processes data in chunks of 3 bytes (24 bits) and converts each chunk into 4 characters (4 × 6 bits).
+func TestGenerateTokenOfCorrectLength(t *testing.T) {
+	length := 3
+	gen := NewTokenGenerator(length)
+	pair, _ := gen.Generate()
+	assert.Equal(t, 4, len(pair.Token))
+}
+
+func TestVerify(t *testing.T) {
+	gen := NewTokenGenerator(3)
+	pair, _ := gen.Generate()
+	assert.True(t, gen.Verify(pair.Token, pair.Hash))
+}
