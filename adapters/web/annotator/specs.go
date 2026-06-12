f789637c691012f74b673b44a7ee49ef20b1481f
lejeunel <me@lejeunel.org>
2026-06-12 14:33:58 +0200

    add image ingestion in annotator

--- adapters/web/annotator/specs.go
+++ adapters/web/annotator/specs.go
@@ -2,8 +2,10 @@ package annotator
 
 import (
 	"fmt"
-	"github.com/lejeunel/go-image-annotator/app/annotator/view"
+	"time"
+
 	html "github.com/lejeunel/go-image-annotator/adapters/web/html"
+	"github.com/lejeunel/go-image-annotator/app/annotator/view"
 	. "maragu.dev/gomponents"
 )
 
@@ -18,6 +20,7 @@ func (p *ImageInfosView) Build(info view.ImageInfo) Node {
 	table.Rows = append(table.Rows, html.SpecTableRow{Name: "mimetype", Value: info.Specs.MIMEType})
 	table.Rows = append(table.Rows, html.SpecTableRow{Name: "dimensions",
 		Value: fmt.Sprintf("%vx%v", info.Specs.Width, info.Specs.Height)})
+	table.Rows = append(table.Rows, html.SpecTableRow{Name: "ingested", Value: info.Specs.IngestedAt.Format(time.DateOnly)})
 	return table.Render()
 
 }
